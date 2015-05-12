f7612f14b974e1a5a91c249cc212ba46d433111f
felicia <quixoticf@gmail.com>
2015-05-12 11:53:47 -0400

    Fill in basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,40 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+   def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+
+  # def self.find_by_credentials(user_params)
+    # user = User.find_by_email(user_params[:email])
+    # user.try(:is_password?(user_params[:password]) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+    def ensure_session_token
+      self.session_token ||= SecureRandom.urlsafe_base64(16)
+    end
 end
