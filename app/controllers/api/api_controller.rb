4dce96ced19e134e4681cb103dfa63c4f5b32485
felicia <quixoticf@gmail.com>
2015-05-13 11:35:31 -0400

    Require user to sign in to navigate site

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,9 @@
 class Api::ApiController < Api::ApplicationController
+  before_action :require_signed_in!
+
+  def require_signed_in!
+    unless signed_in?
+      render json: ["You must be signed in to perform that action."], status: :unauthorized
+    end
+  end
 end
