ae8556b0f322f6289a1ddaf12e5075f838c29402
felicia <quixoticf@gmail.com>
2015-05-20 21:48:09 -0400

    Filter down working, but cannot filter up

--- app/assets/javascripts/views/listings/search_results.js
+++ app/assets/javascripts/views/listings/search_results.js
@@ -2,9 +2,9 @@ Airfnf.Views.SearchResults = Backbone.CompositeView.extend({
   template: JST["listings/search_results"],
 
   events: {
-    'change .price-slider': 'filterPrice',
-    'change .room-type': 'filterRoomType',
-    'change .guests': 'filterGuests'
+    'slidechange .price-slider': 'udpatePrice',
+    'change input:checkbox': 'udpateRoomType',
+    'change .accommodates': 'udpateGuests'
   },
 
   initialize: function () {
@@ -12,24 +12,77 @@ Airfnf.Views.SearchResults = Backbone.CompositeView.extend({
     // add reset event to both listeners?
     this.listenTo(this.collection, "add", this.addListingView);
     this.listenTo(this.collection, "remove", this.removeListingView);
+    //this.listenTo(this.collection, "reset", this.render);
     this.collection.each(this.addListingView.bind(this));
   },
 
-  filterPrice: function (event) {
+  udpatePrice: function (event) {
     event.preventDefault();
-    console.log("HELLO!");
-    // this.collection = Airfnf.currentSearch.where({ price: $(event.currentTarget).val()});
-    // var values = $(".price-slider").slider("values");
+    var values = this.$(".price-slider").slider("values");
+    var newPriceProperty = { price: values }
+
+    this.updateExistingFilters(newPriceProperty);
+    this.filter();
   },
 
-  filterRoomType: function (event) {
+  udpateRoomType: function (event) {
     event.preventDefault();
-    this.collection = Airfnf.currentSearch.where({ roomType: $(event.currentTarget).val()});
+    var roomTypes = this.$('input:checkbox:checked').map(function () { return $(this).val() });
+    var newRoomTypeProperty = { room_type: roomTypes };
+
+    this.updateExistingFilters(newRoomTypeProperty);
+    this.filter();
   },
 
-  filterGuests: function (event) {
+  udpateGuests: function (event) {
     event.preventDefault();
-    this.collection = Airfnf.currentSearch.where({ accommodates: $(event.currentTarget).val()});
+    var guests = this.$(".accommodates").val();
+    var newGuestsProperty = { accommodates: guests };
+
+    this.updateExistingFilters(newGuestsProperty);
+    this.filter();
+  },
+
+  updateExistingFilters: function (property) {
+    this.existingFilters = this.existingFilters || {
+      price: this.$(".price-slider").slider("values"),
+      room_type: ["Entire home/apt", "Private room", "Shared room"],
+      accommodates: 1
+    };
+
+    this.existingFilters = $.extend({}, this.existingFilters, property);
+  },
+
+  filter: function () {
+    //debugger;
+    var newListings = Airfnf.currentSearch.filter(function (listing) {
+      if (this.inPriceRange(listing) && this.inRoomTypes(listing) && this.inGuestRange(listing)) {
+        return listing;
+      }
+    }.bind(this));
+
+    this.collection.set(newListings);
+    //debugger;
+  },
+
+  inPriceRange: function (listing) {
+    var max = this.$(".price-slider").slider("option", "max");
+
+    if ((this.existingFilters.price[0] <= listing.escape("price") &&
+      this.existingFilters.price[1] >= listing.escape("price")) ||
+         (this.existingFilters.price[1] === max && listing.escape("price") > max)) {
+      return true;
+    }
+
+    return false;
+  },
+
+  inRoomTypes: function (listing) {
+    return _.contains(this.existingFilters.room_type, listing.escape("room_type"));
+  },
+
+  inGuestRange: function (listing) {
+    return this.existingFilters.accommodates <= listing.get("accommodates");
   },
 
   addListingView: function (listing) {
@@ -45,6 +98,7 @@ Airfnf.Views.SearchResults = Backbone.CompositeView.extend({
   },
 
   render: function () {
+    console.log("Render!");
     var content = this.template({
       location: this.collection.location
     });
